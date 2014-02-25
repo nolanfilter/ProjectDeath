a724fe02647d0cad11b0312139efe6d49f62d5e5
Nolan Filter <nfilter@ogmento.com>
2014-02-24 20:54:53 -0500

    Added acceleration due to gravity Generalized functions to account for changes in gravity

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -5,7 +5,6 @@ public class CameraController : MonoBehaviour {
 
 	public Transform toFollow = null;
 	public float zOffset = -10f;
-	public float speed = 0.5f;
 
 	void Start()
 	{
@@ -18,6 +17,6 @@ public class CameraController : MonoBehaviour {
 
 	void Update()
 	{
-		transform.position = Vector3.MoveTowards( transform.position, toFollow.position + Vector3.forward * zOffset, speed );
+		transform.position = toFollow.position + Vector3.forward * zOffset;
 	}
 }
