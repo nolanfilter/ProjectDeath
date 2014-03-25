43a46a947aefd11d87379697a1b9cfd15de17aeb
jamessmarion <jamessmarion@gmail.com>
2014-03-24 22:57:31 -0400

    Level2 Physically built.

---
+++ Assets/Scripts/Visual Scripting/Lava.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+
+	SpriteRenderer lightSprite;
+
+	bool increase;
+
+	// Use this for initialization
+	void Start () {
+		lightSprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lightSprite.color = new Color(1,1,1, (Mathf.Sin(Time.time * 1.5f) * .5f + 2.5f) / 5);
+	}
+}
