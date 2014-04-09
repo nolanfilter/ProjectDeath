7969c8d59e11f806363fce0826ed9dc5dbcee2fe
jamessmarion <jamessmarion@gmail.com>
2014-04-09 01:00:28 -0400

    YO MERGE THIS FRLZ

---
+++ Assets/Scripts/Visual Scripting/MenuGlow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGlow : MonoBehaviour {
+
+	SpriteRenderer lightSprite;
+
+	bool increase;
+
+	// Use this for initialization
+	void Start () {
+		lightSprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lightSprite.color = new Color(1,1,1, (Mathf.Sin(Time.time * 3) * .8f + 4f) / 5);
+	}
+}
