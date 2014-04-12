17525132275ad69964848aa35f87ee6ee1838137
Geoffrey Owen <geoaowen999@gmail.com>
2014-04-11 22:26:49 -0400

    engine and kill blocks

---
+++ Assets/Scripts/Controllers/EngineDeathCubeController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineDeathCubeController : MonoBehaviour {
+
+	//public GameObject engine;
+	public bool active;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active == false) {
+			transform.collider.enabled = false;
+		} else {
+			transform.collider.enabled = true;
+		}
+	}
+}
