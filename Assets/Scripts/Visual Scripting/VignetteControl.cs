638df5fa28c0987a241f557f44adc01f562398d1
jamessmarion <jamessmarion@gmail.com>
2014-04-12 22:34:16 -0400

    no message

---
+++ Assets/Scripts/Visual Scripting/VignetteControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VignetteControl : MonoBehaviour {
+	
+	public Color amount;
+
+	private Color change;
+	// Use this for initialization
+	void Start () {
+		change = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		change = Color.Lerp(renderer.material.color, amount, Time.deltaTime * .3f);
+
+		renderer.material.color = change;
+	}
+}
