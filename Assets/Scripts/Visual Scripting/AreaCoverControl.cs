84cc9ef348b533166ad4c88f215af724935f06ab
jamessmarion <jamessmarion@gmail.com>
2014-04-14 12:33:11 -0400

    Visual changes, area masking

---
+++ Assets/Scripts/Visual Scripting/AreaCoverControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaCoverControl : MonoBehaviour {
+
+	public bool on;
+
+	private Color newColor;
+
+	// Use this for initialization
+	void Start () {
+		on = true;
+		if(transform.name == "Janitor")
+		{
+			on = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!on)
+		{
+			newColor = Color.Lerp(renderer.material.color, new Color(1,1,1,1), Time.deltaTime * .3f);
+		}
+		if(on)
+		{
+			newColor = Color.Lerp(renderer.material.color, new Color(0,0,0,0), Time.deltaTime * .6f);
+		}
+
+		renderer.material.color = newColor;
+	}
+}
