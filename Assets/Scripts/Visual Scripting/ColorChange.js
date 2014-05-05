e0efdc324d4a7f034d3ec85bb564c656d2e4984a
jamessmarion <jamessmarion@gmail.com>
2014-05-05 02:25:55 -0400

    buncha changes yo

---
+++ Assets/Scripts/Visual Scripting/ColorChange.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var colors:Color[];
+
+var current:int;
+
+var timer:float;
+
+function Start () {
+	current = 0;
+	timer = 0;
+}
+
+function Update () {
+	timer += Time.deltaTime;
+	
+	if(timer > .2)
+	{
+		renderer.material.color = colors[current];
+		if(current < colors.Length - 1)
+		{
+			current ++;
+		}
+		else
+		{
+			current = 0;
+		}
+		timer = 0;
+	}
+}
---
+++ Assets/Scripts/Visual Scripting/DiscoCover.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	renderer.material.color = new Color(0,0,0, Mathf.Abs((Mathf.Sin(Time.time * 2)) * .5f + 2f) / 7);
+}
