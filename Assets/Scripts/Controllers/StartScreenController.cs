870c5eb7e620ef923e8c09bf26068cd8bb62e102
Geoffrey Owen <geoaowen999@gmail.com>
2014-05-05 22:44:58 -0400

    start screen, new buttons for deaths

--- Assets/Scripts/Controllers/StartScreenController.cs
+++ Assets/Scripts/Controllers/StartScreenController.cs
@@ -5,12 +5,12 @@ public class StartScreenController : MonoBehaviour {
 
 	public GameObject startText;
 	public GameObject creditsText;
-	public GameObject titleText;
+	//public GameObject titleText;
 	public GameObject creditsPrefab;
 
 	private TextMesh start;
 	private TextMesh credits;
-	private TextMesh title;
+	//private TextMesh title;
 
 	private int select = 0;
 	private bool creditsShown = false;
@@ -20,7 +20,7 @@ public class StartScreenController : MonoBehaviour {
 	void Start () {
 		start = startText.GetComponent<TextMesh>();
 		credits = creditsText.GetComponent<TextMesh>();
-		title = titleText.GetComponent<TextMesh>();
+		//title = titleText.GetComponent<TextMesh>();
 	}
 	
 	// Update is called once per frame
@@ -34,21 +34,21 @@ public class StartScreenController : MonoBehaviour {
 
 		if (!creditsShown) {
 			if (select == 0) {
-				start.characterSize = 1.2f;
-				credits.characterSize = 1f;
-				if (Input.GetKeyDown (KeyCode.KeypadEnter)) {
+				start.characterSize = 0.6f;
+				credits.characterSize = 0.5f;
+				if (Input.GetKeyDown (KeyCode.Return)) {
 					Application.LoadLevel("MaybeTheGameMaybe");
 				}
 			}
 			if (select == 1) {
-				credits.characterSize = 1.2f;
-				start.characterSize = 1f;
+				credits.characterSize = 0.6f;
+				start.characterSize = 0.5f;
 				if (Input.GetKeyDown (KeyCode.Return)) {
 					creditsShown = true;
 					start.text = "";
 					credits.text = "";
-					title.text = "";
-					creditsSet = Instantiate(creditsPrefab, new Vector3 (0f, 0f, 0f), Quaternion.identity) as GameObject; //instantiate credits object
+					//title.text = "";
+					creditsSet = Instantiate(creditsPrefab, new Vector3 (0f, 0f, -7f), Quaternion.identity) as GameObject; //instantiate credits object
 				}
 				
 			}
@@ -60,7 +60,7 @@ public class StartScreenController : MonoBehaviour {
 				creditsShown = false;
 				start.text = "Start";
 				credits.text = "Credits";
-				title.text = "Die, Robot";
+				//title.text = "Die, Robot";
 			}
 		}
 	}
